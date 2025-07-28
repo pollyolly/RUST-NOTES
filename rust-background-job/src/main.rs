4d5d29632fae58a42be34b81703720e64df5d5c2
John Mark <johnmarkroco05@gmail.com>
2025-07-28 23:13:53 +0800

    update code

--- rust-background-job/src/main.rs
+++ rust-background-job/src/main.rs
@@ -1,26 +1,15 @@
-use std::sync::mpsc; //multi producer, single consumer
-use std::thread;
+use job_scheduler::{ Job, JobScheduler };
 
 fn main() {
-    let (tx, rx) = mpsc::channel(); //create channel w/ shared queue 
-                                    //return transmitter(txt) and reciever(rx)
-
-    //push data to channel
-    tx.send("Send job to do: 1").unwrap(); //send data to channel
-    tx.send("Another job: 2").unwrap();
-
-    let worker = thread::spawn(move || {
-        loop {
-            let job = rx.recv();//reads/pops data from channel
-            match job {
-                Ok(job) => println!("Job: {}", job),
-                Err(_) => break,
-            }
-        }
-    });
-
-    //push more data to the channel
-    tx.send("Yet another job").unwrap();
-
-    worker.join().unwrap();
+    let mut scheduler = JobScheduler::new();
+/***
+ sec   min   hour   day of month   month   day of week   year
+ *     *     *      *              *       *             *
+***/
+    scheduler.add(Job::new("3 * * * * * *".parse().unwrap(), || {
+        println!("Sending email to existing Subscribes...!");
+    }));
+    loop {
+        scheduler.tick();
+    }
 }
